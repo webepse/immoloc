6d61c265c319ecf8d3d34e8f12e66033920d5cdd
Boderio <boderiotrigan@hotmail.com>
2020-11-09 10:14:59 +0100

    fixtures slug in

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Ad;
+use Faker\Factory;
+use Cocur\Slugify\Slugify;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('FR-fr');
+        $slugify = new Slugify();
+
+        for($a = 1; $a <= 30; $a++){
+            $ad = new Ad();
+            $title = $faker->sentence();
+            $slug = $slugify->slugify($title);
+            $coverImage = $faker->imageUrl(1000,350);
+            $introduction = $faker->paragraph(2);
+            $content = '<p>'.join('</p><p>',$faker->paragraphs(5)).'</p>';
+
+            $ad->setTitle($title)
+                ->setSlug($slug)
+                ->setCoverImage($coverImage)
+                ->setIntroduction($introduction)
+                ->setContent($content)
+                ->setPrice(rand(40,200))
+                ->setRooms(rand(1,5));
+
+            $manager->persist($ad);    
+
+        }
+        $manager->flush();
+    }
+}
