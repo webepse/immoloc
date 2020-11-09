3303d772831835d3cbb72779db5e68c740ed65bb
Boderio <boderiotrigan@hotmail.com>
2020-11-09 11:54:11 +0100

    page d'accueil

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\Ad;
 use Faker\Factory;
 //use Cocur\Slugify\Slugify;
+use App\Entity\Image;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -19,18 +20,28 @@ class AppFixtures extends Fixture
             $ad = new Ad();
             $title = $faker->sentence();
             //$slug = $slugify->slugify($title);
-            $coverImage = $faker->imageUrl(1000,350);
+            //$coverImage = $faker->imageUrl(1000,350);
             $introduction = $faker->paragraph(2);
             $content = '<p>'.join('</p><p>',$faker->paragraphs(5)).'</p>';
 
             $ad->setTitle($title)
-                ->setCoverImage($coverImage)
+                ->setCoverImage('https://picsum.photos/1000/350')
                 ->setIntroduction($introduction)
                 ->setContent($content)
                 ->setPrice(rand(40,200))
                 ->setRooms(rand(1,5));
 
-            $manager->persist($ad);    
+            $manager->persist($ad); 
+            
+            for($i=1; $i <= rand(2,5); $i++){
+                $image = new Image();
+                $image->setUrl('https://picsum.photos/200/200')
+                    ->setCaption($faker->sentence())
+                    ->setAd($ad);
+                $manager->persist($image);    
+            }
+            
+
 
         }
         $manager->flush();
