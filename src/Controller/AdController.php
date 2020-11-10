49e7cb5fe7119b5d0317ed57d7eb2c50a7503c11
Boderio <boderiotrigan@hotmail.com>
2020-11-10 09:56:58 +0100

    annonce v1

---
+++ src/Controller/AdController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Repository\AdRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdController extends AbstractController
+{
+    /**
+     * @Route("/ads", name="ads_index")
+     */
+    public function index(AdRepository $repo): Response
+    {
+        //$repo = $this->getDoctrine()->getRepository(Ad::class);
+
+        $ads = $repo->findAll();
+
+        dump($ads);
+
+        return $this->render('ad/index.html.twig', [
+            'ads' => $ads
+        ]);
+    }
+}
