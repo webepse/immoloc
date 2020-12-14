f8cc20c5065f06137a76e061e3c046409ef234fb
Boderio <boderiotrigan@hotmail.com>
2020-12-14 11:55:14 +0100

    admin ads v1

--- src/Controller/AdminAdController.php
+++ src/Controller/AdminAdController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AdRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,10 @@ class AdminAdController extends AbstractController
     /**
      * @Route("/admin/ads", name="admin_ads_index")
      */
-    public function index(): Response
+    public function index(AdRepository $repo): Response
     {
         return $this->render('admin/ad/index.html.twig', [
-            
+            'ads' => $repo->findAll()
         ]);
     }
 }
