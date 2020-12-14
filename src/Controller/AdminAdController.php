c49fbc1e464c5dc4967441202fbbf47c6309a1cf
Boderio <boderiotrigan@hotmail.com>
2020-12-14 11:17:35 +0100

    admin v1

---
+++ src/Controller/AdminAdController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminAdController extends AbstractController
+{
+    /**
+     * @Route("/admin/ads", name="admin_ads_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('admin/ad/index.html.twig', [
+            
+        ]);
+    }
+}
