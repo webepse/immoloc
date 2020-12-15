6f81b5b0ded6b81e02919ef79a65a1bd20886446
Boderio <boderiotrigan@hotmail.com>
2020-12-15 09:40:34 +0100

    admin ad edit

--- src/Controller/AdminAdController.php
+++ src/Controller/AdminAdController.php
@@ -2,20 +2,57 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
+use App\Form\AnnonceType;
 use App\Repository\AdRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminAdController extends AbstractController
 {
     /**
+     * Permet d'afficher l'ensemble des annonces 
      * @Route("/admin/ads", name="admin_ads_index")
      */
     public function index(AdRepository $repo): Response
     {
         return $this->render('admin/ad/index.html.twig', [
             'ads' => $repo->findAll()
+        ]);
+    }
+
+
+    /**
+     * Permet d'afficher le formulaire d'édition
+     * @Route("/admin/ads/{id}/edit", name="admin_ads_edit")
+     * 
+     * @param Ad $ad
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    public function edit(Ad $ad, Request $request, EntityManagerInterface $manager)
+    {
+        $form = $this->createForm(AnnonceType::class, $ad);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                "success",
+                "L'annonce <strong>{$ad->getTitle()}</strong> a bien été modifiée"
+            );
+        }
+
+        return $this->render("admin/ad/edit.html.twig",[
+            'ad' => $ad,
+            'myForm' => $form->createView()
         ]);
     }
 }
