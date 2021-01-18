a92ab242fb633c31bf76f93bb54a0c3ac5abb59e
berti <berti@CLASSES.LOCAL>
2021-01-18 11:47:04 +0100

    webpack sass - bootstrap

--- assets/app.js
+++ assets/app.js
@@ -6,9 +6,13 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
+
+import $ from 'jquery'
+require('popper.js')
+require('./bootstrap.min.js')
 
 console.log('Hello Webpack Encore! Edit me in assets/app.js');
